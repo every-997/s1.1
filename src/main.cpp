c008f4893b9f8792f7c0306738acc18ae797b47b
every-997 <thomas.every@northumbria.ac.uk>
2018-01-18 10:25:44 +0000

    Cycle of LEDs in functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,31 @@
 #include <mbed.h>
 
+void redLED(){
+  DigitalOut red(PTB22);
+  red.write(0);
+  wait(0.5);
+  red.write(1);
+  wait(0.5);
+}
+void greenLED(){
+  DigitalOut green(PTE26);
+  green.write(0);
+  wait(0.5);
+  green.write(1);
+  wait(0.5);
+}
+void blueLED(){
+  DigitalOut blue(PTB21);
+  blue.write(0);
+  wait(0.5);
+  blue.write(1);
+  wait(0.5);
+}
+
 int main() {
-
-    DigitalOut red(PTB22);
-    DigitalOut green(PTE26);
-    DigitalOut blue(PTB21);
-
     while(1) {
-      red.write(0);
-      wait(0.5);
-      red.write(1);
-      //red cycle
-      wait(0.5);
-      green.write(0);
-      wait(0.5);
-      green.write(1);
-
-      //green cycle
-      wait(0.5);
-      blue.write(0);
-      wait(0.5);
-      blue.write(1);
-      wait(0.5);
-
+      redLED();
+      greenLED();
+      blueLED();
     }
 }
